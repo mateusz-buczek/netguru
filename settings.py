c0d96959e4648ef4c7f87e481ce212fbb543d2eb
Mateusz Buczek <mateusz.buczek89@gmail.com>
2020-10-27 20:42:49 +0100

    env reading

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ print('The .env file has been loaded. See base.py for more information')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env(
+SECRET_KEY = os.getenv(
     "DJANGO_SECRET_KEY",
     default=''
 )
